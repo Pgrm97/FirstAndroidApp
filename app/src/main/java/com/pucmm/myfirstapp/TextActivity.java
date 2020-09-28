1a8ca1738731ca603f92a49cffa67855f9be1dfe
Pgrm97 <Pgrm97@gmail.com>
2020-09-27 21:33:26 -0400

    Added Text Activity

---
+++ app/src/main/java/com/pucmm/myfirstapp/TextActivity.java
@@ -0,0 +1,13 @@
+package com.pucmm.myfirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text);
+    }
+}
